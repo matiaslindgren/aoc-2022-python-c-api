ea742ab954a1d1def3021ce6dcfad75157f7b645
Matias Lindgren <matias.lindgren@iki.fi>
2022-12-28 21:31:15 +0100

    update readme, cleanup d02

--- README.md
+++ README.md
@@ -24,30 +24,32 @@ Nothing intelligent to see here.
     ```
 3. Put inputs under `./txt/inputs/??.txt` and solve (for example day 1) using:
     ```
-    ./solve 1
+    make run_d01
     ```
 
 ## Explanation
 
-`py_aoc` is a program that runs the Python interpreter [entrypoint](./src/py_aoc.c#L105) by [linking](./Makefile#L21) to the [compiled](./Makefile#L48-L50) `libpython3` library:
+`py_aoc` is a program that runs the Python interpreter [entrypoint](./include/solutions_module.h#L231) by [linking](./Makefile#L26) to the [compiled](./Makefile#L53-L57) `libpython3` library:
 ```
 > ./out/py_aoc
-Python 3.11.0 (main, Dec  5 2022, 23:22:09) [GCC 10.2.1 20210110] on linux
+Python 3.11.0 (main, Dec 15 2022, 19:30:38) [Clang 11.0.1 ] on linux
 Type "help", "copyright", "credits" or "license" for more information.
 >>> print("hi")
 hi
 ```
 
-`py_aoc` also contains a [builtin-module `aoc_solve`](./src/py_aoc.c#L88-L90) with a single function named [`y2022`](./src/py_aoc.c#L72-L77), which accepts the day to solve as the only argument.
+`py_aoc` defines a Python builtin-module named [`aoc_solve`](./include/solutions_module.h#L151-L157) with a single function named [`y2022`](./include/solutions_module.h#L135-L140).
+The function takes a single integer argument between 1 to 25 for the Advent of Code 2022 challenge to solve.
 
-For example, solving day 1:
+For example, solving day 2:
 ```
-> ./out/py_aoc -c "from aoc_solve import y2022; print(y2022(1))"
-68442 204837
+> ./out/py_aoc -c "from aoc_solve import y2022; print(y2022(2))"
+0.4 ms
+15632 14416
 ```
 
 Steps in more detail:
-1. [Read](./include/common.h#L35) [UTF-8 input](./txt/input/01.txt) into a [single `str`](./src/py_aoc.c#L46:L50).
-2. Call [the solver of day 1](./src/py_aoc.c#L15:L16) by passing the input as a string argument.
-3. [Compute](./include/d01.h#L5) numerical solutions to part 1 and 2, then [format](./include/d01.h#L53) them into a single space-separated `str`.
-4. Cleanup [refcounts](https://docs.python.org/3/c-api/intro.html#reference-counts), return the solution and print to stdout.
+1. [Read](./include/solutions_module.h#L103) [UTF-8 input](./txt/input/01.txt) into a single Python `str`.
+2. Pass input `str` as argument to the [solver function](./include/solutions_module.h#L35:L36).
+3. [Compute](./include/d02.h#L9) numerical solutions to part 1 and 2, then [format](./include/d02.h#L58) the numbers into a single space-separated `str`.
+4. Cleanup refcounts, return the solution, and print to stdout.
--- include/d02.h
+++ include/d02.h
@@ -7,56 +7,58 @@ int _AoC_y2022_d02_encode_hand(int rock, int paper, int scissors) {
 }
 
 PyObject *AoC_y2022_d02(PyObject *unicode_input) {
+  PyObject *solution = 0;
   PyObject *lines = PyUnicode_Splitlines(unicode_input, 0);
   if (!lines) {
     PyErr_Format(PyExc_RuntimeError, "could not split input to lines");
-    goto error;
+    goto done;
   }
 
   const long draw_points = 3;
   const long win_points = 6;
   long part1 = 0;
   long part2 = 0;
-  Py_ssize_t num_lines = PyList_Size(lines);
+
+  const Py_ssize_t num_lines = PyList_Size(lines);
+
   for (Py_ssize_t i = 0; i < num_lines; ++i) {
-    PyObject *line = PyList_GetItem(lines, i);
-    Py_INCREF(line);
+    PyObject *line = Py_NewRef(PyList_GetItem(lines, i));
     PyObject *pair = AoC_unicode_split(line, " ", 1);
-    Py_DECREF(line);
-    PyObject *lhs = PyList_GetItem(pair, 0);
-    Py_INCREF(lhs);
-    PyObject *rhs = PyList_GetItem(pair, 1);
-    Py_INCREF(rhs);
-    Py_DECREF(pair);
-    int lhs_a = AoC_PyUnicode_Equals_ASCII(lhs, "A");
-    int lhs_b = AoC_PyUnicode_Equals_ASCII(lhs, "B");
-    int lhs_c = AoC_PyUnicode_Equals_ASCII(lhs, "C");
-    Py_DECREF(lhs);
-    int rhs_x = AoC_PyUnicode_Equals_ASCII(rhs, "X");
-    int rhs_y = AoC_PyUnicode_Equals_ASCII(rhs, "Y");
-    int rhs_z = AoC_PyUnicode_Equals_ASCII(rhs, "Z");
-    Py_DECREF(rhs);
-    int lhs_hand = _AoC_y2022_d02_encode_hand(lhs_a, lhs_b, lhs_c);
-    int rhs_hand = _AoC_y2022_d02_encode_hand(rhs_x, rhs_y, rhs_z);
-    int hand_to_win_lhs = lhs_hand % 3 + 1;
-    int hand_to_lose_lhs = (lhs_hand + 1) % 3 + 1;
+    PyObject *lhs = Py_NewRef(PyList_GetItem(pair, 0));
+    PyObject *rhs = Py_NewRef(PyList_GetItem(pair, 1));
+
+    const int lhs_a = AoC_PyUnicode_Equals_ASCII(lhs, "A");
+    const int lhs_b = AoC_PyUnicode_Equals_ASCII(lhs, "B");
+    const int lhs_c = AoC_PyUnicode_Equals_ASCII(lhs, "C");
+
+    const int rhs_x = AoC_PyUnicode_Equals_ASCII(rhs, "X");
+    const int rhs_y = AoC_PyUnicode_Equals_ASCII(rhs, "Y");
+    const int rhs_z = AoC_PyUnicode_Equals_ASCII(rhs, "Z");
+
+    const int lhs_hand = _AoC_y2022_d02_encode_hand(lhs_a, lhs_b, lhs_c);
+    const int rhs_hand = _AoC_y2022_d02_encode_hand(rhs_x, rhs_y, rhs_z);
+
+    const int hand_to_win_lhs = lhs_hand % 3 + 1;
+    const int hand_to_lose_lhs = (lhs_hand + 1) % 3 + 1;
+
     part1 += rhs_hand;
     part1 += draw_points * (rhs_hand == lhs_hand);
     part1 += win_points * (rhs_hand == hand_to_win_lhs);
+
     part2 += rhs_x * hand_to_lose_lhs;
     part2 += rhs_y * (draw_points + lhs_hand);
     part2 += rhs_z * (win_points + hand_to_win_lhs);
+
+    Py_DECREF(line);
+    Py_DECREF(pair);
+    Py_DECREF(lhs);
+    Py_DECREF(rhs);
   }
-  Py_DECREF(lines);
 
-  PyObject *part1_py = PyLong_FromLong(part1);
-  PyObject *part2_py = PyLong_FromLong(part2);
-  PyObject *solution = PyUnicode_FromFormat("%S %S", part1_py, part2_py);
-  Py_DECREF(part1_py);
-  Py_DECREF(part2_py);
+  solution = PyUnicode_FromFormat("%ld %ld", part1, part2);
+
+done:
+  Py_XDECREF(lines);
   return solution;
-
-error:
-  return NULL;
 }
 #endif  // _AOC_Y2022_D02_H_INCLUDED
